512a72bd10bed3240151e125360ddc6824c40922
Prajapati <bprajapati@deloitte.com>
2017-12-20 14:07:12 -0500

    Initial Load

---
+++ src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -0,0 +1,46 @@
+package guru.springframework.bootstrap;
+
+import guru.springframework.domain.Product;
+import guru.springframework.repositories.ProductRepository;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class ProductLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+    private ProductRepository productRepository;
+
+    private Logger log = Logger.getLogger(ProductLoader.class);
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        Product shirt = new Product();
+        shirt.setDescription("BAPS Aksharpith Shirt");
+        shirt.setPrice(new BigDecimal("18.95"));
+        shirt.setImageUrl("/resources/static/images/image1.jpg");
+        shirt.setProductId("235268845711068308");
+        productRepository.save(shirt);
+
+        log.info("Saved Shirt - id: " + shirt.getId());
+
+        Product mug = new Product();
+        mug.setDescription("BAPS Aksharpith Mug");
+        mug.setImageUrl("/resources/static/images/image1.png");
+        mug.setProductId("168639393495335947");
+        mug.setPrice(new BigDecimal("11.95"));
+        productRepository.save(mug);
+
+        log.info("Saved Mug - id:" + mug.getId());
+    }
+}
